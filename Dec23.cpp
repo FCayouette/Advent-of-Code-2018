78d803804febf7ea3c168f7e51fa0b4c8d3e1e12
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2019-01-07 21:45:32 -0500

    Better Day 23 solution
    Replaced std::set by a heap out of a std::vector for part 2 as we do not need full ordering of the data structure. Simplifies Voxel::operator< and reduce memory usage. Profiling indicates this is slightly faster although slightly more verbose

--- Dec23.cpp
+++ Dec23.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <set>
 #include <algorithm>
+#include <chrono>
 
 template <typename T>
 constexpr void GrowToEncompass(T& min, T& max, T val)
@@ -21,18 +21,17 @@ struct Nanobot
 
 struct Voxel
 {
-	Voxel(int x, int X, int y, int Y, int z, int Z) : minX(x), maxX(X), minY(y), maxY(Y), minZ(z), maxZ(Z), score(0), scale(X-x + 1)
-		, distance((std::abs(X + x) + std::abs(Y + y) + std::abs(Z + z)) / 2){} // Manhattan distance from origin to the Voxel's center
+	Voxel(int x, int X, int y, int Y, int z, int Z) : minX(x), maxX(X), minY(y), maxY(Y), minZ(z), maxZ(Z), score(0), scale(X - x + 1)
+		, distance((std::abs(X + x) + std::abs(Y + y) + std::abs(Z + z)) / 2) {} // Manhattan distance from origin to the Voxel's center
 
-	void CheckBot(const Nanobot& b) 
+	void CheckBot(const Nanobot& b)
 	{	// Checks if bot range touches the voxel
 		if (b.range - std::max(minX - b.x, 0) - std::max(b.x - maxX, 0) - std::max(minY - b.y, 0) - std::max(b.y - maxY, 0)
-			- std::max(minZ - b.z, 0) - std::max(b.z - maxZ, 0) >= 0) 
+			- std::max(minZ - b.z, 0) - std::max(b.z - maxZ, 0) >= 0)
 			++score;
 	}
 
-	bool operator<(const Voxel& v) const { return score > v.score || (score == v.score && (distance < v.distance || (distance == v.distance && (scale < v.scale || (scale == v.scale &&
-		(minX < v.minX || (minX == v.minX && (minY < v.minY || (minY == v.minY && minZ < v.minZ))))))))); }
+	bool operator<(const Voxel& v) const { return score < v.score || (score == v.score && (distance > v.distance || (distance == v.distance && scale > v.scale))); }
 
 	int minX, minY, minZ, maxX, maxY, maxZ; // Range values are inclusive
 	int score, scale, distance;
@@ -49,13 +48,13 @@ int main(int argc, char* argv[])
 
 	std::ifstream in(argv[1], std::ios::in);
 	std::vector<Nanobot> bots;
-	
+
 	int maxRange = -1;
 	int maxRangeBot = -1;
-	
+
 	int _a, _b, _c, _r;
 
-	int minX =0, minY=0, minZ=0, maxX=0, maxY=0, maxZ=0;
+	int minX = 0, minY = 0, minZ = 0, maxX = 0, maxY = 0, maxZ = 0;
 	std::string line;
 	while (std::getline(in, line))
 	{
@@ -70,9 +69,9 @@ int main(int argc, char* argv[])
 		GrowToEncompass(minY, maxY, _b);
 		GrowToEncompass(minZ, maxZ, _c);
 	}
-	
-	std::cout << "Part 1: " << std::count_if(bots.cbegin(), bots.cend(), [b = bots[maxRangeBot]](const Nanobot& n) { return b.InRange(n); }) << std::endl;
 
+	std::cout << "Part 1: " << std::count_if(bots.cbegin(), bots.cend(), [b = bots[maxRangeBot]](const Nanobot& n) { return b.InRange(n); }) << std::endl;
+	
 	int maxDimension = std::max(maxX - minX, std::max(maxY - minY, maxZ - minZ));
 	int sampling = 1;
 	while (maxDimension >= 2)
@@ -82,22 +81,24 @@ int main(int argc, char* argv[])
 	}
 
 	// Create initial voxels
-	std::set<Voxel> voxels;
+	std::vector<Voxel> voxels;
 	for (int x = minX; x < maxX; x += sampling)
 		for (int y = minY; y < maxY; y += sampling)
 			for (int z = minZ; z < maxZ; z += sampling)
 			{
-				Voxel v(x, x + sampling-1, y, y + sampling-1, z, z + sampling-1);
+				Voxel v(x, x + sampling - 1, y, y + sampling - 1, z, z + sampling - 1);
 				for (const Nanobot& bot : bots)
 					v.CheckBot(bot);
-				voxels.insert(v);
+				voxels.push_back(v);
 			}
+	std::make_heap(voxels.begin(), voxels.end());
 
 	// Split best voxel until best voxel is only a single point
 	while (voxels.cbegin()->scale > 1)
 	{
 		Voxel bestVoxel = *voxels.begin();
-		voxels.erase(voxels.begin());
+		std::pop_heap(voxels.begin(), voxels.end());
+		voxels.pop_back();
 		// Split the old voxel
 		int newScale = bestVoxel.scale / 2;
 		for (int x = bestVoxel.minX; x < bestVoxel.maxX; x += newScale)
@@ -107,10 +108,11 @@ int main(int argc, char* argv[])
 					Voxel v(x, x + newScale - 1, y, y + newScale - 1, z, z + newScale - 1);
 					for (const Nanobot& bot : bots)
 						v.CheckBot(bot);
-					voxels.insert(v);
+					voxels.push_back(v);
+					std::push_heap(voxels.begin(), voxels.end());
 				}
 	}
-
+	
 	std::cout << "Part 2: " << voxels.cbegin()->distance << std::endl;
 	return 0;
 }
